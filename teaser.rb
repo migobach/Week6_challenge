761ccf7b00613ee2055ec67b98191bd9117fd667
Michael Bach <migobach@gmail.com>
2018-06-17 15:54:01 -0600

    add working function

--- teaser.rb
+++ teaser.rb
@@ -1,30 +1,33 @@
 require 'pry'
 
-def mostLetters
-  wordsArr = []
-  most = []
-
-  puts "Enter a word or phrase: "
-  input = gets.strip.split(' ')
-
-  input.each do |s|
-    uniqWord = s.split('')
-    wordsArr << uniqWord
-  end
-
-  wordsArr.each do |letter|
-    if letter.uniq!
-    most << val
-    # return val.to_i
-  end
-
-  count the uniq letters and delete that from the word length
-
-  binding.pry
-
-
+def start 
+puts "Enter a word or phrase"
+@input = gets.strip.split(' ')
+mostLetters
 end
 
-mostLetters
+def mostLetters
+wordsArr = []
+most = 0
+repeatsPresent = ''
 
-#arr.count
+  @input.each do |w|
+    length = w.length
+    uniqWord = w.split('').uniq.length
+    repeated = length - uniqWord
+    if repeated > most
+      most = repeated
+      repeatsPresent = w
+    elsif
+      repeated == most
+      repeatsPresent = repeatsPresent + ', ' + w 
+    end
+  end 
+  if most == 0 
+    puts "No words with repeats"
+  else
+    puts repeatsPresent + " has/have the most repeated letters."
+  end
+end
+
+start
